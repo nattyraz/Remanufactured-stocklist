c55e333e8936b2b1909007e01dc41db1417216bb
ROFOX <rgfogliami@gmail.com>
2023-08-14 10:44:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,32 @@
-
 import streamlit as st
 import pandas as pd
 
-# Path to the initial Excel file
-FILE_PATH = "template price list.xlsx"
-
-# Cache the initial loading of the Excel file to improve performance
-@st.cache
-def load_data():
-    return pd.read_excel(FILE_PATH)
-
-# Function to display the DataFrame in Streamlit
+# Fonction pour afficher le DataFrame dans Streamlit
 def display_data(df):
     st.write(df)
 
-# Main application
+# Application principale
 def main():
     st.title("Gestionnaire de liste de prix")
     
-    # Display the data
-    df = load_data()
-    display_data(df)
+    # Charger un fichier pour afficher et mettre à jour les données
+    uploaded_file = st.file_uploader("Importez un fichier pour affichage et mise à jour:", type=["xlsx"])
     
-    # Allow user to upload a new file to update the data
-    uploaded_file = st.file_uploader("Importez un nouveau fichier de stock pour mettre à jour:", type=["xlsx"])
     if uploaded_file is not None:
-        df_new = pd.read_excel(uploaded_file)
-        display_data(df_new)
-        st.success("Les données ont été mises à jour avec succès!")
+        try:
+            df = pd.read_excel(uploaded_file)
+            
+            # Vérifiez que le fichier chargé a la même structure que le fichier modèle
+            # (Vous pouvez ajuster cette vérification selon vos besoins)
+            expected_columns = ["No.", "Description", "Item Category Code", "Product Group Code", "Software Language", 
+                                "Keyboard Language", "Graphics01", "Graphics02", "Condition", "Warranty", "Avail. Qty", "Promo Price EUR"]
+            if list(df.columns) == expected_columns:
+                display_data(df)
+            else:
+                st.error("La structure du fichier chargé ne correspond pas au modèle attendu!")
+        
+        except Exception as e:
+            st.error(f"Une erreur s'est produite lors de la lecture du fichier: {e}")
 
 if __name__ == "__main__":
     main()
