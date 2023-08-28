1eb635c27862705f431ff774e78f412f8506f2c6
ROFOX <rgfogliami@gmail.com>
2023-08-28 20:54:34 +0200

    Update pie_chart.py

--- pie_chart.py
+++ pie_chart.py
@@ -10,6 +10,10 @@ st.set_page_config(
     layout="wide"
 )
 
+# Constants
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin"
+
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def get_combined_data():
     return {'data': None}
@@ -17,14 +21,6 @@ def get_combined_data():
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def get_last_update_date():
     return {'date': None}
-
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def get_user_ips():
-    return []
-
-@st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def get_online_users():
-    return set()
 
 def advanced_filter_data_by_search_query(df, query):
     sub_queries = re.split(r'[ *]', query)
@@ -36,53 +32,23 @@ def advanced_filter_data_by_search_query(df, query):
     return df
 
 def display_data_page():
-    user_ip = st.session_state.get("user_ip", "unknown")
-    online_users = get_online_users()
-    online_users.add(user_ip)
+    # (Same code as before...)
 
-    col1, col2 = st.columns([1, 6])
-    with col1:
-        st.image("https://github.com/nattyraz/Remanufactured-stocklist/blob/main/logo%20foxway.png?raw=true", width=100)
-    with col2:
-        st.title("Remanufactured stocklist Lenovo Garantie Original")
-    
-    combined_data = get_combined_data()['data']
-    last_update_date = get_last_update_date()['date']
-    
-    if last_update_date:
-        st.write(f"Last update: {last_update_date.strftime('%Y-%m-%d %H:%M:%S')}")
-    
-    search_query = st.text_input("Search by description or No. (use the * in your searches):")
-    
-    if search_query:
-        combined_data = advanced_filter_data_by_search_query(combined_data, search_query)
-
-    if combined_data is not None and not combined_data.empty:
-        # ... [rest of the code as is]
+def check_credentials(username, password):
+    return username == ADMIN_USERNAME and password == ADMIN_PASSWORD
 
 def admin_page():
     st.title("Administration")
-
-    # Admin authentication
-    password = st.sidebar.text_input("Enter admin password:", type='password')
-    if password != "secret_password":  # Replace "secret_password" with your own password
-        st.error("Access denied. Invalid password!")
-        return
     
-    # Display online users
-    online_users = get_online_users()
-    st.write(f"Users currently online: {len(online_users)}")
-
-    # ... [rest of the code as is]
+    username = st.text_input("Nom d'utilisateur", type="default")
+    password = st.text_input("Mot de passe", type="password")
+    
+    if check_credentials(username, password):
+        # (Same code as before for file uploads...)
+    else:
+        st.warning("Identifiants incorrects. Veuillez réessayer.")
 
 def main():
-    # Check and record user IP
-    user_ip = st.request.headers.get("client-ip", "unknown")
-    st.session_state.user_ip = user_ip
-    user_ips = get_user_ips()
-    if user_ip not in user_ips:
-        user_ips.append(user_ip)
-
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Choisissez une page:", ["Affichage des données", "Administration"])
     
