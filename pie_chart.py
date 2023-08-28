bc88f58479bcdcb0cf5fb179d825a2bc574adf71
ROFOX <rgfogliami@gmail.com>
2023-08-28 20:12:35 +0200

    Update pie_chart.py

--- pie_chart.py
+++ pie_chart.py
@@ -1,20 +1,14 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime, timedelta
-import re
+from datetime import datetime
+import re  # For regular expression matching
 
-# Configuration de la page
+# Set page configuration
 st.set_page_config(
     page_title="Remanufactured Stocklist",
     page_icon="favicon.ico",
     layout="wide"
 )
-
-# Variables globales
-LOGIN = "admin"
-PASSWORD = "admin"
-user_online_timestamp = datetime.now() - timedelta(days=1)
-user_email = None
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
 def get_combined_data():
@@ -25,8 +19,12 @@ def get_last_update_date():
     return {'date': None}
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
-def get_user_count():
-    return {'count': 0}
+def get_user_ips():
+    return []
+
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def get_online_users():
+    return set()
 
 def advanced_filter_data_by_search_query(df, query):
     sub_queries = re.split(r'[ *]', query)
@@ -37,75 +35,60 @@ def advanced_filter_data_by_search_query(df, query):
             df = df[df.apply(lambda row: row.astype(str).str.contains(pattern).any(), axis=1)]
     return df
 
-# Fonction manquante is_user_online()
-def is_user_online():
-    global user_online_timestamp
-    return (datetime.now() - user_online_timestamp).seconds < 300
-
 def display_data_page():
-    global user_online_timestamp, user_email
-    user_online_timestamp = datetime.now()
-    get_user_count()['count'] += 1
+    user_ip = st.session_state.get("user_ip", "unknown")
+    online_users = get_online_users()
+    online_users.add(user_ip)
 
     col1, col2 = st.columns([1, 6])
     with col1:
         st.image("https://github.com/nattyraz/Remanufactured-stocklist/blob/main/logo%20foxway.png?raw=true", width=100)
     with col2:
         st.title("Remanufactured stocklist Lenovo Garantie Original")
-
+    
     combined_data = get_combined_data()['data']
     last_update_date = get_last_update_date()['date']
-
+    
     if last_update_date:
         st.write(f"Last update: {last_update_date.strftime('%Y-%m-%d %H:%M:%S')}")
+    
+    search_query = st.text_input("Search by description or No. (use the * in your searches):")
+    
+    if search_query:
+        combined_data = advanced_filter_data_by_search_query(combined_data, search_query)
 
-    if combined_data is None:
-        st.warning("Le DataFrame 'combined_data' est None.")
-    elif combined_data.empty:
-        st.warning("Le DataFrame 'combined_data' est vide.")
-    else:
-        st.success("Le DataFrame 'combined_data' est chargé avec succès.")
-        
-        search_query = st.text_input("Search by description or No. (use the * in your searches):")
-        if search_query:
-            combined_data = advanced_filter_data_by_search_query(combined_data, search_query)
-        
-        st.dataframe(combined_data)
-
-    email_input = st.text_input("Entrez votre e-mail pour recevoir des mises à jour:")
-    if email_input:
-        user_email = email_input
-        st.success("E-mail enregistré avec succès!")
+    if combined_data is not None and not combined_data.empty:
+        # ... [rest of the code as is]
 
 def admin_page():
-    global user_email
-
     st.title("Administration")
-    
-    if "logged_in" not in st.session_state or not st.session_state.logged_in:
-        entered_login = st.text_input("Login:")
-        entered_password = st.text_input("Password:", type="password")
-        if st.button("Se connecter"):
-            if entered_login == LOGIN and entered_password == PASSWORD:
-                st.session_state.logged_in = True
-                st.success("Connexion réussie!")
-                st.experimental_rerun()
-            else:
-                st.error("Login ou mot de passe incorrect!")
-        return
 
-    if is_user_online():
-        st.warning("Un utilisateur est actuellement en ligne!")
-    if user_email:
-        st.info(f"L'utilisateur avec l'e-mail {user_email} souhaite recevoir des mises à jour.")
+    # Admin authentication
+    password = st.sidebar.text_input("Enter admin password:", type='password')
+    if password != "secret_password":  # Replace "secret_password" with your own password
+        st.error("Access denied. Invalid password!")
+        return
+    
+    # Display online users
+    online_users = get_online_users()
+    st.write(f"Users currently online: {len(online_users)}")
+
+    # ... [rest of the code as is]
 
 def main():
+    # Check and record user IP
+    user_ip = st.request.headers.get("client-ip", "unknown")
+    st.session_state.user_ip = user_ip
+    user_ips = get_user_ips()
+    if user_ip not in user_ips:
+        user_ips.append(user_ip)
+
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Choisissez une page:", ["Affichage des données", "Administration"])
-
+    
     if page == "Affichage des données":
         display_data_page()
-    elif page == "Administration":
+    else:
         admin_page()
 
 if __name__ == "__main__":
