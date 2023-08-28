53b4ca63890e3d25580b56dc4d27fac3d6b4ba6f
ROFOX <rgfogliami@gmail.com>
2023-08-28 20:17:35 +0200

    Update pie_chart.py

--- pie_chart.py
+++ pie_chart.py
@@ -60,7 +60,7 @@ def display_data_page():
     if combined_data is not None and not combined_data.empty:
         # ... [rest of the code as is]
 
-def admin_page():
+ddef admin_page():
     st.title("Administration")
 
     # Admin authentication
