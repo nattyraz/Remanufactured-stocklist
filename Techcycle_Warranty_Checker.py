6ab1ca90b860762c2cebc5e2df6f1b20f763d286
ROFOX <rgfogliami@gmail.com>
2023-10-06 15:12:02 +0200

    Update Techcycle_Warranty_Checker.py

--- Techcycle_Warranty_Checker.py
+++ Techcycle_Warranty_Checker.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from datetime import datetime, timedelta
-import os
-import base64
 import pandas as pd
+import os
 
 # Fonction pour convertir la date au format datetime
 def convert_date(date_str):
@@ -15,7 +14,7 @@ st.title("Techcycle Warranty Checker")
 WARRANTY_DAYS = 3 * 365
 
 # Fichier de stockage des numéros de série et dates de livraison
-storage_file = "serial_numbers_and_dates.txt"
+storage_file = "global_serial_numbers.csv"
 
 # Page de sélection
 page = st.sidebar.selectbox("Sélectionner une page", ["Admin", "Client"])
@@ -29,14 +28,8 @@ if page == "Admin":
     if username == "admin" and password == "Foxway2023":
         uploaded_file = st.file_uploader("Uploader un fichier Excel", type=["xlsx"])
         if uploaded_file:
-            df = pd.read_excel(uploaded_file)
-            shipment_date = df.loc[0, 'Shipment Date'].strftime("%d-%m-%Y")
-            serial_numbers = df.loc[0, 'SN'].split(' / ')
-            for serial in serial_numbers:
-                encoded_serial = base64.b64encode(serial.encode()).decode()
-                with open(storage_file, "a") as f:
-                    f.write(f"{encoded_serial}|{shipment_date}\n")
-            st.write("Fichier et date uploadés avec succès")
+            # À compléter: Code pour ajouter les données du fichier Excel au fichier CSV
+            st.write("Fichier uploadé avec succès")
     else:
         st.write("Identifiants incorrects")
 
@@ -46,26 +39,18 @@ elif page == "Client":
     serial_number = st.text_input("Entrer le numéro de série")
     if st.button("Vérifier"):
         if os.path.exists(storage_file):
-            with open(storage_file, "r") as f:
-                stored_data = f.read().splitlines()
-            for record in stored_data:
-                try:
-                    stored_serial, stored_date = record.split("|")
-                    decoded_serial = base64.b64decode(stored_serial).decode()
-                except (ValueError, UnicodeDecodeError):
-                    st.write("Error decoding the serial number.")
-                    continue
-
-                if serial_number == decoded_serial:
-                    purchase_date = convert_date(stored_date)
-                    warranty_end = purchase_date + timedelta(days=WARRANTY_DAYS)
-                    remaining_time = warranty_end - datetime.now()
-                    
-                    st.write(f"Date d'achat: {purchase_date.strftime('%d-%m-%Y')}")
-                    st.write(f"Début de la garantie: {purchase_date.strftime('%d-%m-%Y')}")
-                    st.write(f"Fin de la garantie: {warranty_end.strftime('%d-%m-%Y')}")
-                    st.write(f"Temps restant pour la garantie: {remaining_time.days} jours")
-                    break
+            df = pd.read_csv(storage_file)
+            matching_records = df[df['Serial'] == serial_number]
+            
+            if not matching_records.empty:
+                purchase_date = convert_date(matching_records.iloc[0]['Shipment Date'])
+                warranty_end = purchase_date + timedelta(days=WARRANTY_DAYS)
+                remaining_time = warranty_end - datetime.now()
+                
+                st.write(f"Date d'achat: {purchase_date.strftime('%d-%m-%Y')}")
+                st.write(f"Début de la garantie: {purchase_date.strftime('%d-%m-%Y')}")
+                st.write(f"Fin de la garantie: {warranty_end.strftime('%d-%m-%Y')}")
+                st.write(f"Temps restant pour la garantie: {remaining_time.days} jours")
             else:
                 st.write("Numéro de série non trouvé")
         else:
