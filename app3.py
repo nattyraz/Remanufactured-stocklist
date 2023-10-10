46534e2955dbd334ec2fe9219278c368019b85ee
ROFOX <rgfogliami@gmail.com>
2023-10-10 20:29:26 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -5,8 +5,7 @@ import re  # For regular expression matching
 import io
 
 # Constants for Admin Authentication
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "foxway2023"
+
 
 def check_credentials(username, password):
     return username == ADMIN_USERNAME and password == ADMIN_PASSWORD
