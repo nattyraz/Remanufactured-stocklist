97dcbc0931c12fc7a6092673c0726022c2fed923
ROFOX <rgfogliami@gmail.com>
2023-10-11 13:50:43 +0200

    Update app_modified.py

--- app_modified.py
+++ app_modified.py
@@ -4,8 +4,8 @@ from datetime import datetime
 import re  # For regular expression matching
 
 # Constants for Admin Authentication
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "foxway2023"
+admin_username = st.secrets["general"]["ADMIN_USERNAME"]
+admin_password = st.secrets["general"]["ADMIN_PASSWORD"]
 
 def check_credentials(username, password):
     return username == ADMIN_USERNAME and password == ADMIN_PASSWORD
