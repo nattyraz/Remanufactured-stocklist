7ba6e0a7107c33acf921d46d09d440d0ecbb23c5
ROFOX <rgfogliami@gmail.com>
2024-05-01 13:32:56 +0200

    Update cust.py

--- cust.py
+++ cust.py
@@ -44,15 +44,5 @@ if not data.empty:
         ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         st.pyplot(fig1)
     
-    # Bar chart for clients by city
-    if 'By' in data.columns and st.checkbox("Voir le nombre de clients par ville"):
-        city_counts = data['By'].value_counts()
-        fig2, ax2 = plt.subplots()
-        city_counts.plot(kind='bar', color='skyblue', ax=ax2)
-        ax2.set_title('Nombre de Clients par Ville')
-        ax2.set_xlabel('Ville')
-        ax2.set_ylabel('Nombre de Clients')
-        st.pyplot(fig2)
-
 else:
     st.write("Veuillez télécharger un fichier pour voir les données.")
